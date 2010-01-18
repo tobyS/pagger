edfef04f799250989469f2d57221855dc193f56e
Tobias Schlitt <tobias@schlitt.info>
2010-01-18 09:22:43 +0000

    - Added main program script.
    git-svn-id: svn+ssh://hartes-php.de/var/svn/pagger@8 4c45de2a-1fe6-423d-8495-111c8ba1ceba

---
+++ trunk/pagger.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.path.append('./src')
+
+# import lastfm.service
+# import lastfm.artist
+# import lastfm.track
+# 
+# service = lastfm.service.Service('682587831457dcf13f569c79b930d866')
+# artist = lastfm.artist.Artist(service, '2 Unlimited')
+# track = lastfm.track.Track(service, artist, 'Get ready for this')
+# for tag in track.get_top_tags():
+# 	print(tag.__dict__)
+
+from program.main import Main
+
+main = Main()
+main.run()
