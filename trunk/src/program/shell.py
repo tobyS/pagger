c0ce064c8530c66e369ef4315b8493f48c4e746c
Tobias Schlitt <tobias@schlitt.info>
2010-01-21 13:25:31 +0000

    - WS.
    git-svn-id: svn+ssh://hartes-php.de/var/svn/pagger@16 4c45de2a-1fe6-423d-8495-111c8ba1ceba

--- trunk/src/program/shell.py
+++ trunk/src/program/shell.py
@@ -5,61 +5,78 @@ import shlex
 
 class Shell (cmd.Cmd):
 
-	_handler = None
+    _handler = None
 
-	_config = None
+    _config = None
 
-	def __init__(self, handler, config):
-		cmd.Cmd.__init__(self)
-		self._handler = handler
-		self._config = config
+    def __init__(self, handler, config):
+        cmd.Cmd.__init__(self)
+        self._handler = handler
+        self._config = config
 
-	def postcmd(self, stop, line):
-		return stop
+    def postcmd(self, stop, line):
+        return stop
 
-	def do_map(self, s):
-		params = shlex.split(s)
-		
-		if len(params) != 2:
-			return self._error(u'Expected exactly 2 parameters.')
+    # 'map' command
 
-		from_tag = params[0]
-		to_tag = params[1]
-		
-		if to_tag not in self._config.tags:
-			return self._error(u'Tag "' + to_tag + u'" not recognized.')
-		
-		print u'Established new mapping "' + from_tag + u'" → "' + to_tag + u'"'
+    def do_map(self, s):
+        params = shlex.split(s)
+        
+        if len(params) != 2:
+            return self._error(u'Expected exactly 2 parameters.')
 
-	def help_map(self):
-		print 'Adds a new tag mapping'
+        from_tag = params[0]
+        to_tag = params[1]
+        
+        if to_tag not in self._config.tags:
+            return self._error(u'Tag "' + to_tag + u'" not recognized.')
+        
+        print u'Established new mapping "' + from_tag + u'" → "' + to_tag + u'"'
 
-	def do_avail(self, s):
-		print u'Available tags'
-		print u'--------------'
-		for tag in self._config.tags:
-			print tag
+    def help_map(self):
+        print 'Adds a new tag mapping'
 
-	def help_avail(self):
-		print u'Lists all defined tags'
+    # 'avail' command
 
-	def do_tags(self, s):
-		print u'Assigned tags'
-		print u'-------------'
-		for tag in self._handler.get_tags():
-			print tag
+    def do_avail(self, s):
+        print u'Available tags'
+        print u'--------------'
+        for tag in self._config.tags:
+            print tag
 
-	def help_tags(self):
-		print u'Displays the tags found for the track'
+    def help_avail(self):
+        print u'Lists all defined tags'
 
-	def do_q(self, s):
-		return True
+    # 'tags' command
 
-	def help_q(self):
-		print 'Quits the shell. Note that you need to save if changes should not be lost.'
+    def do_tags(self, s):
+        print u'Assigned tags'
+        print u'-------------'
+        for tag in self._handler.get_tags():
+            print tag
 
-	def help_help(self):
-		print 'Prints available commands. Use "help <command>" to get command help.'
+    def help_tags(self):
+        print u'Displays the tags found for the track'
 
-	def _error(self, message):
-		print u'Error: ' + message
+    # 'q' command
+
+    def do_quit(self, s):
+        return True
+
+    def help_quit(self):
+        print 'Quits the shell. Note that you need to save if changes should not be lost.'
+
+    do_q = do_quit
+    help_q = help_quit
+
+    do_EOF = do_quit
+
+    # 'help' command
+
+    def help_help(self):
+        print 'Prints available commands. Use "help <command>" to get command help.'
+
+    # misc
+
+    def _error(self, message):
+        print u'Error: ' + message
