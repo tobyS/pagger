f91d350e0c5bc25d6fd558807d9e00a7580e42e0
Tobias Schlitt <tobias@schlitt.info>
2010-01-21 05:30:46 +0000

    - Moved the API key here now, since it should be application specific. # Not sure if I should leave that later, but makes instantiation of the Service # class easier now.
    git-svn-id: svn+ssh://hartes-php.de/var/svn/pagger@10 4c45de2a-1fe6-423d-8495-111c8ba1ceba

--- trunk/src/lastfm/service.py
+++ trunk/src/lastfm/service.py
@@ -2,13 +2,9 @@
 import urllib
 
 class Service:
-	_api_key = ""
+	_api_key = '682587831457dcf13f569c79b930d866'
 
-	_base_url = u"http://ws.audioscrobbler.com/2.0/"
-
-	def __init__(self, api_key):
-		self._api_key = api_key
-
+	_base_url = u'http://ws.audioscrobbler.com/2.0/'
 
 	def call(self, class_name, method_name, params):
 		params['method'] = class_name + '.' + method_name
