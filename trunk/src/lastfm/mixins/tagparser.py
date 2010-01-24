55da92e25de9b2c6523fd51f704f9cc3e91246c6
Tobias Schlitt <tobias@schlitt.info>
2010-01-24 15:52:27 +0100

    - Handle exception if XML response from Last.FM could not be parsed.   (Print an error for now, should be handled more gracefully.) - WS.

--- trunk/src/lastfm/mixins/tagparser.py
+++ trunk/src/lastfm/mixins/tagparser.py
@@ -4,19 +4,23 @@ from lastfm.tag import Tag
 
 class TagParser:
 
-	def parse_tags(self, xml):
-		dom = parseString(xml)
-		tags = []
-		for tag_element in dom.getElementsByTagName('tag'):
-			tags.append(self._parse_tag(tag_element))
-		return tags
+    def parse_tags(self, xml):
+        try:
+            dom = parseString(xml)
+        except xml.parsers.expat.ExpatError:
+            print u'Could not parse XML response "' + xml + '"'
+            return []
+        tags = []
+        for tag_element in dom.getElementsByTagName('tag'):
+            tags.append(self._parse_tag(tag_element))
+        return tags
 
-	def _parse_tag(self, tag_element):
-		return Tag(
-				tag_element.getElementsByTagName('name').item(0).firstChild.nodeValue,
-				tag_element.getElementsByTagName('url').item(0).firstChild.nodeValue,
-				int(tag_element.getElementsByTagName('count').item(0).firstChild.nodeValue)
-					if tag_element.getElementsByTagName('count').length > 0
-					else -1
-		)
+    def _parse_tag(self, tag_element):
+        return Tag(
+                tag_element.getElementsByTagName('name').item(0).firstChild.nodeValue,
+                tag_element.getElementsByTagName('url').item(0).firstChild.nodeValue,
+                int(tag_element.getElementsByTagName('count').item(0).firstChild.nodeValue)
+                    if tag_element.getElementsByTagName('count').length > 0
+                    else -1
+        )
 
