ff3d3e608534b5e6455452d35d492e6fcf162139
Tobias Schlitt <tobias@schlitt.info>
2010-02-01 23:10:09 +0100

    - WS.

--- trunk/src/lastfm/service.py
+++ trunk/src/lastfm/service.py
@@ -1,14 +1,13 @@
-
 import urllib
 
 class Service:
-	_api_key = '682587831457dcf13f569c79b930d866'
+    _api_key = '682587831457dcf13f569c79b930d866'
 
-	_base_url = u'http://ws.audioscrobbler.com/2.0/'
+    _base_url = u'http://ws.audioscrobbler.com/2.0/'
 
-	def call(self, class_name, method_name, params):
-		params['method'] = class_name + '.' + method_name
-		params['api_key'] = self._api_key
-		return urllib.urlopen(
-			self._base_url + '?' + urllib.urlencode(params)
-		).read()
+    def call(self, class_name, method_name, params):
+        params['method'] = class_name + '.' + method_name
+        params['api_key'] = self._api_key
+        return urllib.urlopen(
+            self._base_url + '?' + urllib.urlencode(params)
+        ).read()
