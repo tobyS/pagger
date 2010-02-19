731c4e9dea5d2b981a19b0fa29a472276f8d7e98
Tobias Schlitt <tobias@schlitt.info>
2010-02-19 09:26:15 +0100

    - Trying to finally fix unicode handling in config file handler.

--- trunk/src/config.py
+++ trunk/src/config.py
@@ -30,12 +30,12 @@ class Config:
 
     def load_file(self, file):
         dom = parse(file)
-        self._parse_settings(dom.getElementsByTagName('settings').item(0))
-        self.tags = self._parse_set(dom.getElementsByTagName('tags').item(0), 'tag', unicode)
+        self._parse_settings(dom.getElementsByTagName(u'settings').item(0))
+        self.tags = self._parse_set(dom.getElementsByTagName(u'tags').item(0), u'tag', unicode)
         for tag in self.tags:
             self.tagmap[tag.lower()] = tag
-        self.ignore = self._parse_set(dom.getElementsByTagName('ignore').item(0), 'match', unicode)
-        self._parse_tagmap(dom.getElementsByTagName('tagmap').item(0))
+        self.ignore = self._parse_set(dom.getElementsByTagName(u'ignore').item(0), u'match', unicode)
+        self._parse_tagmap(dom.getElementsByTagName(u'tagmap').item(0))
         self._changed = False
 
     def add_tag(self, tag):
@@ -49,25 +49,25 @@ class Config:
 
     def set(self, key, val):
         if key not in self._available_settings:
-            raise RuntimeError('Invalid setting "' + key + '".')
+            raise RuntimeError(u'Invalid setting "' + key + u'".')
         self.settings[key] = self._available_settings[key](val)
         self._changed = True
 
     def save(self):
         impl = getDOMImplementation()
-        xml = impl.createDocument(None, 'config', None)
+        xml = impl.createDocument(None, u'config', None)
         config_elem = xml.documentElement
 
-        settings_elem = config_elem.appendChild(xml.createElement('settings'))
+        settings_elem = config_elem.appendChild(xml.createElement(u'settings'))
         self._save_settings(xml, settings_elem)
 
-        tags_elem = config_elem.appendChild(xml.createElement('tags'))
-        self._save_set(xml, tags_elem, 'tag', self.tags)
+        tags_elem = config_elem.appendChild(xml.createElement(u'tags'))
+        self._save_set(xml, tags_elem, u'tag', self.tags)
 
-        ignore_elem = config_elem.appendChild(xml.createElement('ignore'))
-        self._save_set(xml, ignore_elem, 'match', self.ignore)
+        ignore_elem = config_elem.appendChild(xml.createElement(u'ignore'))
+        self._save_set(xml, ignore_elem, u'match', self.ignore)
 
-        tagmap_elem = config_elem.appendChild(xml.createElement('tagmap'))
+        tagmap_elem = config_elem.appendChild(xml.createElement(u'tagmap'))
         self._save_mappings(xml, tagmap_elem, self.tagmap)
 
         xml.writexml(open(self._file, 'w'), '', '  ', "\n", 'UTF-8')
@@ -79,23 +79,23 @@ class Config:
     def _save_settings(self, xml, parent):
         for key, val in self.settings.items():
             setting_elem = parent.appendChild(xml.createElement(key))
-            setting_elem.appendChild(xml.createTextNode(str(val)))
+            setting_elem.appendChild(xml.createTextNode(unicode(val).encode('utf-8')))
 
     def _save_mappings(self, xml, parent, dict):
         for key, val in dict.items():
             if key == val.lower():
                 continue
-            mapping_elem = parent.appendChild(xml.createElement('mapping'))
-            from_elem = mapping_elem.appendChild(xml.createElement('from'))
-            from_elem.appendChild(xml.createTextNode(key))
-            to_elem = mapping_elem.appendChild(xml.createElement('to'))
-            to_elem.appendChild(xml.createTextNode(val))
+            mapping_elem = parent.appendChild(xml.createElement(u'mapping'))
+            from_elem = mapping_elem.appendChild(xml.createElement(u'from'))
+            from_elem.appendChild(xml.createTextNode(unicode(key).encode('utf-8')))
+            to_elem = mapping_elem.appendChild(xml.createElement(u'to'))
+            to_elem.appendChild(xml.createTextNode(unicode(val).encode('utf-8')))
 
 
     def _save_set(self, xml, parent, tag_name, set):
         for item in set:
             elem = parent.appendChild(xml.createElement(tag_name))
-            elem.appendChild(xml.createTextNode(str(item)))
+            elem.appendChild(xml.createTextNode(unicode(item).encode('utf-8')))
 
     def _parse_settings(self, settings):
         for node in settings.childNodes:
@@ -118,6 +118,6 @@ class Config:
     def _parse_tagmap(self, tagmap):
         for node in tagmap.childNodes:
             if node.nodeType == node.ELEMENT_NODE:
-                from_val = node.getElementsByTagName('from').item(0).firstChild.nodeValue.strip()
-                to_val = node.getElementsByTagName('to').item(0).firstChild.nodeValue.strip()
+                from_val = node.getElementsByTagName(u'from').item(0).firstChild.nodeValue.strip()
+                to_val = node.getElementsByTagName(u'to').item(0).firstChild.nodeValue.strip()
                 self.add_mapping(from_val, to_val)
